aa769c69ecacec02964f8f06dbe0d6492569a5a1
ThatOSDeveloper <owen.winchell@proton.me>
2025-01-01 19:04:57 -0500

    Moved to CC0 public domain license

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,6 +2,7 @@
 
 void kernel_main() {
     printf("Thank you for using SlugOS\n");
+    printf("SlugOS is a public domain FOSS operating system.\n");
     // Halt our kernel since nothing left to do
     asm("cli; hlt");
 }
