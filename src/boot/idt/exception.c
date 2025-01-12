93301ab990985749e77239295d04403d8ce2387e
ThatOSDeveloper <owen.winchell@proton.me>
2025-01-12 10:55:17 -0500

    updated exception handler to print something when called

--- src/boot/idt/exception.c
+++ src/boot/idt/exception.c
@@ -1,6 +1,8 @@
+#include <slug.h>
+
 __attribute__((noreturn))
-void exception_handler(void);
 void exception_handler() {
+    writestring("Exception handler triggered\n");
     __asm__ volatile ("cli; hlt"); // Completely hangs the computer
     while(1);
 }
