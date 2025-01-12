3e8b6697eaf8c107cb8d77a4668171105789eda4
ThatOSDeveloper <owen.winchell@proton.me>
2025-01-12 10:28:47 -0500

    fixed the fact that terminal_initialize is called before the stack is made (but somehow works?)

--- src/headers/slug.h
+++ src/headers/slug.h
@@ -4,6 +4,8 @@
 The equivalent to dos.h in our case it contains the basics that are needed
 */
 
+#include <stdint.h>
+
 // Return status stuff
 
 typedef int slug_status; // similar to EFI_STATUS
@@ -23,3 +25,13 @@ void putchar(char c);
 void writestring(const char* data);
 
 void printf(const char *fmt, ...);  // A printf implementation
+
+// Output to ports needed for stuff like a PS/2 driver or interacting with pretty much any hardware
+
+// 8bit data being recivied/sent
+void outb(uint16_t port, uint8_t val);
+uint8_t inb(uint16_t port);
+
+// 16bit data being recivied/sent
+void outw(uint16_t port, uint16_t val);
+uint16_t inw(uint16_t port);
