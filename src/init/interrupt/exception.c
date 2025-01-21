b464384fc4bb5062db7d051e427957f9a7fbb91d
ThatOSDeveloper <owen.winchell@proton.me>
2025-01-20 20:19:33 -0500

    readded in borked code for interrupt handling

--- src/init/interrupt/exception.c
+++ src/init/interrupt/exception.c
@@ -1,10 +1,68 @@
 #include <slug.h>
+#include <stdint.h>
+
+// This contains all of the registers and other info that is passed from the interrupt
+typedef struct {
+    uint32_t ds;       // Data segment selector
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // General-purpose registers
+    uint32_t int_num;  // Interrupt number (which exception triggered this handler)
+    uint32_t err_code; // Error code (only relevant for certain exceptions)
+    uint32_t eip;      // Instruction pointer at the moment of the exception
+    uint32_t cs;       // Code segment at the moment of the exception
+    uint32_t eflags;   // Flags register
+    uint32_t useresp;  // Stack pointer in user mode (if applicable)
+    uint32_t ss;       // Stack segment (if applicable)
+} exception_frame_t;
+
+// Every type of exception that can happen
+const char* exception_messages[] = {
+    "Divide By Zero",
+    "Debug",
+    "Non-maskable Interrupt",
+    "Breakpoint",
+    "Overflow",
+    "Bound Range Exceeded",
+    "Invalid Opcode",
+    "Device Not Available",
+    "Coprocessor Segment Overrun",
+    "Invalid TSS",
+    "Segment Not Present",
+    "Stack-Segment Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Reserved",
+    "x87 Floating-Point Exception",
+    "Alignment Check",
+    "Machine Check",
+    "SIMD Floating-Point Exception",
+    "Virtualization Exception",
+    "Control Protection Exception",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Hypervisor Injection Exception",
+    "VMM Communication Exception",
+    "Security Exception",
+    "Reserved",
+    "FPU Error Interrupt",
+};
+
+const char* get_exception_message(uint32_t int_num) {
+    if (int_num < sizeof(exception_messages) / sizeof(exception_messages[0])) {
+        return exception_messages[int_num];
+    } else {
+        return "Unknown Exception";
+    }
+}
 
 __attribute__((noreturn))
-void exception_handler() {
-    printk("Exception handler triggered\n");
+void exception_handler(exception_frame_t *frame) {
+    printk("Exception handler triggered: %s\n Exception %d\n", get_exception_message(frame->int_num), frame->int_num);
     #ifdef DEBUG
-    // Do a stack trace if debug mode is enabled
+    // Do a stack trace
     TraceStackTrace(10);
     #endif
     __asm__ volatile ("cli; hlt"); // Completely hangs the computer
--- src/kernel.c
+++ src/kernel.c
@@ -2,4 +2,7 @@
 
 void kernel_main(void) {
 	printk("Thank you for using SlugOS\n");
+	#ifdef DEBUG
+	asm("int $0");
+	#endif
 }
