b568631232d33c82c6c994493d87879c04f373de
ThatOSDeveloper <owen.winchell@proton.me>
2025-01-20 20:11:30 -0500

    removed no longer needed debug code

--- src/init/interrupt/exception.c
+++ src/init/interrupt/exception.c
@@ -4,7 +4,7 @@ __attribute__((noreturn))
 void exception_handler() {
     printk("Exception handler triggered\n");
     #ifdef DEBUG
-    // Do a stack trace
+    // Do a stack trace if debug mode is enabled
     TraceStackTrace(10);
     #endif
     __asm__ volatile ("cli; hlt"); // Completely hangs the computer
--- src/kernel.c
+++ src/kernel.c
@@ -1,27 +1,5 @@
 #include <slug.h>
-#ifdef DEBUG
-#include <liballoc.h>
-#include <string.h>
-#endif
-
-#ifdef DEBUG
-int liballoc_test() {
-	void* data = malloc(5);
-	if (data == NULL) {
-		return 1;
-	}
-	strcpy(data, "hi!\n");
-	printk("Liballoc test: %s", data);
-	free(data);
-	return 0;
-}
-#endif
 
 void kernel_main(void) {
 	printk("Thank you for using SlugOS\n");
-	#ifdef DEBUG
-	// test liballoc
-	liballoc_test();
-	TraceStackTrace(10);
-	#endif
 }
