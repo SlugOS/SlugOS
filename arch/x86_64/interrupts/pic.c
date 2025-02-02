b51851f8a636117aa657ccb1b2a6ca3e09303d34
ThatOSDeveloper <owen.winchell@proton.me>
2025-02-02 14:20:28 -0500

    refactor PIC interrupt handling: reintroduce pic_disable function and add IRQ_clear_mask for better IRQ management

--- arch/x86_64/interrupts/pic.c
+++ arch/x86_64/interrupts/pic.c
@@ -7,14 +7,6 @@
 #define PIC2_COMMAND	PIC2
 #define PIC2_DATA	(PIC2+1)
 
-// Mask all of the PIC IRQs
-void pic_disable(void) {
-    outb(PIC1_DATA, 0xff);
-    outb(PIC2_DATA, 0xff);
-}
-
-// We still need to remap it because of the spurious IRQs that it can still generate
-
 #define ICW1_ICW4	0x01		/* Indicates that ICW4 will be present */
 #define ICW1_SINGLE	0x02		/* Single (cascade) mode */
 #define ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
@@ -26,6 +18,12 @@ void pic_disable(void) {
 #define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
 #define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
 #define ICW4_SFNM	0x10		/* Special fully nested (not) */
+
+// Mask all of the PIC IRQs
+void pic_disable(void) {
+    outb(PIC1_DATA, 0xff);
+    outb(PIC2_DATA, 0xff);
+}
 
 void PIC_remap(int offset1, int offset2) {
 	uint8_t a1, a2;
@@ -53,4 +51,18 @@ void PIC_remap(int offset1, int offset2) {
 	
 	outb(PIC1_DATA, a1);   // restore saved masks.
 	outb(PIC2_DATA, a2);
+}
+
+void IRQ_clear_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);        
 }
